4bdb5c116f8751bcce3c7cf07734c7cec03eb6cf
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-08-21 20:36:07 +0530

    ..

--- src/app/api/auth/signup.ts
+++ src/app/api/auth/signup.ts
@@ -1,14 +1,10 @@
-import { error } from "console";
 import connectMongo from "../../../lib/conn";
 import Users from "../../../../model/Schema";
-import {hash} from "bcryptjs"
+import { hash } from "bcryptjs";
 import { NextApiRequest, NextApiResponse } from "next";
-
 
 export default async function handler(req : NextApiRequest, res:NextApiResponse){
     connectMongo().catch(error => res.json({error:"Connnection failed..!"}))
-
-
 
     if(req.method == "POST"){
         if(!req.body) return res.status(404).json({error:"Dont have form data"});
@@ -19,14 +15,13 @@ export default async function handler(req : NextApiRequest, res:NextApiResponse)
         const checkexisiting = await Users.findOne({email});
         if(checkexisiting) return res.status(422).json({message: "User alreasy Exists..!"})
 
-
         Users.create({firstname, lastname, email, password : await hash(password, 12)}, function(err:any,data:any){
             if(err) return res.status(400).json({err})
             res.status(201).json({status:true, user: data})
         })
 
-
     }else{
         res.status(500).json({message: "HTTP method not valid only POST method valid"})
     }
 }
+

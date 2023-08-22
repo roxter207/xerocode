c38110b1d42ce3263d1b32972a5efcdeebdba2f6
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-08-22 14:43:15 +0530

    styln

--- src/component/Userinfo.tsx
+++ src/component/Userinfo.tsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import { DefaultSession } from "next-auth";
-// import { signOut } from "next-auth/react";
-// import Image from "next/image";
-
-// const Userinfo = ({ user }: { user: DefaultSession["user"] }) => {
-//   async function handleGoogleSignOut() {
-//     signOut({ callbackUrl: "http://localhost:3000/login" });
-//   }
-
-//   return (
-//     <>
-//       {/* <div>Userinfo: {user?.name}</div>
-//       <div>Userinfo: {user?.email}</div>
-
-//       <button onClick={handleGoogleSignOut}>signout</button> */}
-
-//       <div className="bg-white flex-col justify-center align-content:center w-full h-[100vh] text-black">
-//         <div className="flex content-center">
-//           <Image src="/logo.png" alt="logo" width="150" height="1" />
-//         </div>
-//         <h1 className="text-center font-bold text-2xl pt-5">
-//           Welcome <span>{user?.name}</span>
-//         </h1>
-//         <div className="grid grid-cols-3 pt-5 items-end mb-6 w-[759.5px] m-auto ">
-//           <div className=" h-1 border-t-2 rounded border-[#AAB2C873] border-opacity-40"></div>
-//           <div className="text-center text-lg font-light">
-//             Choose from the Following
-//           </div>
-//           <div className=" h-1 border-t-2 rounded border-[#AAB2C873] border-opacity-40"></div>
-//         </div>
-//         <div className="flex space-x-10">
-//           <button>Developer</button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Userinfo;
-
 import React, { useState } from "react";
 import { DefaultSession } from "next-auth";
 import { signOut } from "next-auth/react";
@@ -67,9 +26,10 @@ const Userinfo = ({ user }: { user: DefaultSession["user"] }) => {
     // Handle submission of the input value
     console.log("Submitted:", inputValue);
   };
+  const [isHovering, setIsHovering] = useState(false);
 
   return (
-    <div className="bg-white flex-col justify-center align-content:center w-full h-[100vh] text-black">
+    <div className="bg-white flex-col justify-center align-content:center w-full h-[100vh] space-y-2 text-black">
       <div className="flex content-center">
         <Image src="/logo.png" alt="logo" width="150" height="1" />
       </div>
@@ -83,28 +43,49 @@ const Userinfo = ({ user }: { user: DefaultSession["user"] }) => {
         </div>
         <div className="h-1 border-t-2 rounded border-[#AAB2C873] border-opacity-40"></div>
       </div>
-      <div className="flex justify-center space-x-4">
+      <div className="flex justify-center space-x-10">
         <button
-          className={`border py-2 px-4 rounded-lg ${
-            selectedOption === "developer" ? "bg-blue-500 text-black" : "bg-transparent"
-          }`}
-          onClick={() => handleOptionClick("developer")}
+          style={{
+            width: 150,
+            height: 50,
+            margin: 20,
+            border:'1px solid',
+            borderRadius: 5,
+            backgroundColor:
+              selectedOption === "Developer" ? "blue" : "transparent",
+            color: selectedOption === "Developer" ? "white" : "black",
+          }}
+          onClick={() => handleOptionClick("Developer")}
         >
           Developer
         </button>
         <button
-          className={`border py-2 px-4 rounded-lg ${
-            selectedOption === "organization" ? "bg-blue-500 text-black" : "bg-transparent"
-          }`}
-          onClick={() => handleOptionClick("organization")}
+          style={{
+            width: 150,
+            height: 50,
+            margin: 20,
+            borderRadius: 5,
+            border:'1px solid',
+            backgroundColor:
+              selectedOption === "Organization " ? "blue" : "transparent",
+            color: selectedOption === "Organization " ? "white" : "black",
+          }}
+          onClick={() => handleOptionClick("Organization ")}
         >
           Organization
         </button>
         <button
-          className={`border py-2 px-4 rounded-lg ${
-            selectedOption === "company" ? "bg-blue-500 text-black" : "bg-transparent"
-          }`}
-          onClick={() => handleOptionClick("company")}
+          style={{
+            width: 150,
+            height: 50,
+            margin: 20,
+            borderRadius: 5,
+            border:'1px solid',
+            backgroundColor:
+              selectedOption === "Company" ? "blue" : "transparent",
+            color: selectedOption === "Company" ? "white" : "black",
+          }}
+          onClick={() => handleOptionClick("Company")}
         >
           Company
         </button>
@@ -114,24 +95,34 @@ const Userinfo = ({ user }: { user: DefaultSession["user"] }) => {
           <input
             className="border rounded-md py-2 px-3 focus:outline-none focus:ring focus:border-blue-300"
             type="text"
-            placeholder={selectedOption}
+            placeholder={selectedOption+" Name"}
             value={inputValue}
             onChange={handleInputChange}
+            style={{ marginTop: 150, padding: 15 }}
           />
           <button
             className="mt-2 bg-blue-500 text-black py-1 px-4 rounded-md hover:bg-blue-600"
             onClick={handleSubmit}
+            style={{
+              width: 100,
+              height: 50,
+              margin: 20,
+              backgroundColor: "blue",
+              color: "white",
+            }}
           >
             Submit
           </button>
         </div>
       )}
-      <button
-        className="mt-4 bg-red-500 text-black py-1 px-4 rounded-md hover:bg-red-600"
-        onClick={handleGoogleSignOut}
-      >
-        Sign Out
-      </button>
+      {user && (
+        <button
+          className="mt-4 bg-red-500 text-black py-1 px-4 rounded-md hover:bg-red-600"
+          onClick={handleGoogleSignOut}
+        >
+          Sign Out
+        </button>
+      )}
     </div>
   );
 };

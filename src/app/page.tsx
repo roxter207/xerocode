1802716ece94bd4700ea146d12cc61b331230ff8
roxter207 <66411952+roxter207@users.noreply.github.com>
2023-08-22 19:14:56 +0530

    trying my bes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@
 
 import Userinfo from '@/component/Userinfo'
 import { useSession } from 'next-auth/react'
-
-
-
+import LoginPage from './login/page'
 
 export default function Home() {
   const {data:session} = useSession();
 
   return(
+    
     <Userinfo user={session?.user}/>
   )
 }
